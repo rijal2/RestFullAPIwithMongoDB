6fe6de69f22cd1b898cab832856b4433ee196c64
rijal2 <merentas5benua27@gmail.com>
2022-05-26 23:29:16 +0700

    02. Melakuka testing terhadap file entri point, dengan menampilkan informasi port yang sedang dijalankan oleh sistem

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RestFullAPIwithMongoDB
 Membuat RestFull API dengan database yang tersimpan di MONGODB
+
+01. Install module= express, mongoose, cors
+02. Buat file entri point app.js dan lakukan inisialisasi dengan npm init
+03. Lakukan set konfigurasi pada file app.js
--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => {
+    res.json({
+        message: "Selamat Belajar",
+        id: "1234"
+    })
+})
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Sistem sedang berjalan pada http://localhost:${PORT}`)
+})
